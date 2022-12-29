94b1e4d05d4be68824254255fd5a891d2b966def
Eldar Ulanov <eldar.ulanov_2025@ucentralasia.org>
2022-12-29 19:08:21 +0600

    initial commit

---
+++ src/main/java/libraryApp/Main.java
@@ -0,0 +1,34 @@
+package libraryApp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import libraryApp.Utils.FxmlUtils;
+
+import java.util.Locale;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        //Locale.setDefault(new Locale("en")); // Uncomment for english version of app
+
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/mainBorderPane.fxml"));
+        loader.setResources(FxmlUtils.getResource());
+        BorderPane mainPane = loader.load();
+        Scene mainScene = new Scene(mainPane);
+        primaryStage.setTitle(FxmlUtils.getResource().getString("title.application"));
+        primaryStage.setScene(mainScene);
+        primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("/icons/app_icon.png")));
+        primaryStage.show();
+    }
+}
